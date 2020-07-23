f74f2dd6fe9a127ae5e538be2c8432d4ad6685c7
Brandon <defoe.brand@gmail.com>
2020-07-23 18:04:30 +0300

    Create my_each and my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -1,8 +1,26 @@
-.each
-.each_with_index
-.all?
-.any?
-.none?
-.count
-.map
-.inject
+hash = { type: 'tree', fruit: false, count: 3 }
+array = [5, 4, 3, 2, 1, 'hello', 'world']
+
+module Enumerable
+  def my_each
+    ind = 0
+    length.times do
+      yield(self[ind])
+      ind += 1
+    end
+  end
+
+  def my_each_with_index
+    ind = 0
+    length.times do
+      yield(self[ind], ind)
+      ind += 1
+    end
+  end
+end
+
+# array.each { |val| p "Hello, this is my value: #{val}" }
+array.my_each { |val| p "Hello, this is my value: #{val}" }
+
+# array.each_with_index { |val, ind| p "Hello, this is my value: #{val} at index: #{ind}" }
+array.my_each_with_index { |val, ind| p "Hello, this is my value: #{val} at index: #{ind}" }
